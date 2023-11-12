ac9c83b616a386d9ef13558e7dfc129c4a8b55d8
Eric Tossell <erictossell@gmail.com>
2023-11-12 15:29:06 -0500

    nixtop addition

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
-   nixbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICIyO3jhe9heIjuUnW6MP/0U3/qjG63kTmM2wRp5nht1";
-   nixboard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxslzX9LKcpUevEafRPlT4lgcRIGCNrwHBGJ1tiNdBU";
-   nixcube = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC1c3qwjqf3eWNl7N1OYjoqa6estljz+VBpeSV9kIhXD";
-   systems = [ nixbox nixboard nixcube ];
+  nixboard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxslzX9LKcpUevEafRPlT4lgcRIGCNrwHBGJ1tiNdBU";
+  nixbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICIyO3jhe9heIjuUnW6MP/0U3/qjG63kTmM2wRp5nht1";
+  nixcube = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC1c3qwjqf3eWNl7N1OYjoqa6estljz+VBpeSV9kIhXD";
+  nixtop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJS6X46jQ0Wy5wtJsIbpnmRKS2B2zFrBuQ1JRbELxix+";
+  systems = [ nixboard nixbox nixcube nixtop ];
 in
 {
   "wireless.age".publicKeys = systems;

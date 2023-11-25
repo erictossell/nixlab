47259d5b47da532594fe3855c9657df754534254
Eric Tossell <erictossell@gmail.com>
2023-11-25 03:37:20 -0500

    updated grafana

--- modules/monitoring/grafana/default.nix
+++ modules/monitoring/grafana/default.nix
@@ -3,7 +3,7 @@
   services.grafana = {
     enable = true;
     settings.server = {
-      http_addr = "127.0.0.1";
+      http_addr = "10.0.0.197";
       http_port = 3000;
       domain = "nixcube.et";
     };

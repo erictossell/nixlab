77c3b0787c1da24ef46373333771f284121b0bde
Eric Tossell <erictossell@gmail.com>
2023-12-07 17:49:40 -0500

    Updated nixbox

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   eriim = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBh6+743NINanzS+X6QUbhhj/XN2edYxiD7TrUNclQN1";
   users = [ eriim ];
   nixboard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9scH83gPs0dbiliNHvnRjZDo4hCwe3R0W26SN4f7Jb";
-  nixbox = "ssh-ed25519 AAAC3NzaC1lZDI1NTE5AAAIAhKvn+by6FngLSbbTdXKPAirdEAaSHnK8IQGXyXF/jQ";
+  nixbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAhKvn+by6FngLSbbTdXKPAirdEAaSHnK8IQGXyXF/jQ";
   nixcube = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC1c3qwjqf3eWNl7N1OYjoqa6estljz+VBpeSV9kIhXD";
   systems = [ nixboard nixbox nixcube ];
 in

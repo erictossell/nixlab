428d2effe81c3e855a626242798e5c98b5c3cad6
Eric Tossell <erictossell@gmail.com>
2023-12-12 09:30:10 -0500

    Docker container declaration

--- modules/actual/default.nix
+++ modules/actual/default.nix
@@ -1,27 +1,23 @@
-{ config, pkgs, user, ... }:
-let
-  data_path = "/srv/actual-data/";
-  compose_path = "/home/${user}/actual";
-in
-{
-  environment.etc."${compose_path}/compose.yaml".source = ./compose.yaml;
-
-  systemd.services.actualServer = {
-    serviceConfig = {
-      Environment = "PATH=/run/current-system/sw/bin";
-      Type = "simple";
-      Restart = "always";
-    };
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" "docker.service" ];
-    script = ''
-      cd ${compose_path}
-      ${pkgs.docker-compose}/bin/docker-compose up --detach
-    '';
-    preStop = ''
-      cd ${compose_path}
-      ${pkgs.docker-compose}/bin/docker-compose down
-    '';
-  };
-
+{ config, pkgs, ... }:
+ {
+   config.virtualisation.oci-containers.containers = {
+     actual = {
+       image = "actualbudget/actual-server:latest";
+       ports = ["127.0.0.1:8443:5006"];
+       volumes = [
+         "/srv/actual-data:/data"
+	 "/srv/actual-data/cert.crt:/data/cert.crt"
+	 "/srv/actual-data/key.key:/data/key.key"
+       ];
+       environment = {
+         ACTUAL_HTTPS_KEY="/data/key.key";
+	 ACTUAL_HTTPS_CERT="/data/cert.crt";
+       };
+       cmd = [
+         "--pull=always"
+         "--restart=unless-stopped"
+	 "-d"
+       ];
+     };
+   };
 }

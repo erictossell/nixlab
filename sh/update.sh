7f3453a6f1ee4453922d1b27ef5de9dab43511fa
Eric Tossell <erictossell@gmail.com>
2023-12-29 00:14:09 -0500

    Updated deploy scripts

--- sh/update.sh
+++ sh/update.sh
@@ -4,7 +4,9 @@
 SERVERS=("192.168.2.195" "192.168.2.196" "192.168.2.197")
 
 # Command to execute
-COMMAND="sudo nixos-rebuild switch --flake 'github:erictossell/nix-pi-lab'"
+COMMAND="sudo nixos-rebuild switch --flake github:erictossell/nix-pi-lab"
+
+CLEAN="nix-collect-gargage -d"
 
 # SSH Options
 SSH_OPTIONS="-p 2973"
@@ -12,6 +14,8 @@ SSH_OPTIONS="-p 2973"
 # Loop through the servers and execute the command
 for SERVER in "${SERVERS[@]}"; do
     echo "Executing on $SERVER"
+    ssh $SSH_OPTIONS eriim@$SERVER "$CLEAN"
+
     ssh $SSH_OPTIONS eriim@$SERVER "$COMMAND"
 done
 

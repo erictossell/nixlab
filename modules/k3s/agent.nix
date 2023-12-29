a243a987f54bce886882c28bc8c81dc9b91f82f0
Eric Tossell <erictossell@gmail.com>
2023-12-29 12:14:49 -0500

    k3s binary only

--- modules/k3s/agent.nix
+++ modules/k3s/agent.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 {
-  services.k3s = {
-    enable = true;
-    role = "server";
-    token = "myrandomizedtoken";
-    serverAddr = "https://192.168.2.195:6443";
-  };
+  #services.k3s = {
+  #  enable = true;
+  #  role = "server";
+  #  token = "myrandomizedtoken";
+  #  serverAddr = "https://192.168.2.195:6443";
+  #};
   networking.firewall = {
     allowedTCPPorts = [ 2379 2380 6443 10250 ];
     allowedUDPPorts = [ 8472 51820 51821 ];
--- modules/k3s/default.nix
+++ modules/k3s/default.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 {
-  services.k3s = {
-    enable = true;
-    role = "server";
-    token = "myrandomizedtoken";
-    clusterInit = true;
-  };
+  #services.k3s = {
+  #  enable = true;
+  #  role = "server";
+  #  token = "myrandomizedtoken";
+  #  clusterInit = true;
+  #};
   networking.firewall = {
     allowedTCPPorts = [ 2379 2380 6443 10250 ];
     allowedUDPPorts = [ 8472 51820 51821 ];

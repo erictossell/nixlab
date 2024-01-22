1484ccf793e74d92ed9219f743098f3bcf2c4cde
Eric Tossell <erictossell@gmail.com>
2024-01-22 13:02:13 -0500

    .local

--- modules/grafana/default.nix
+++ modules/grafana/default.nix
@@ -6,13 +6,13 @@
       server = {
         http_addr = "0.0.0.0";
 	http_port = 3001;
-        domain = "${hostName}";
-	root_url = "http://${hostName}/grafana/"; 
+        domain = "${hostName}.local";
+	root_url = "http://${hostName}.local/grafana/"; 
 	serve_from_sub_path = true;
       };
     };
   };
-  services.nginx.virtualHosts.${hostName} = {
+  services.nginx.virtualHosts."${hostName}.local" = {
   addSSL = true;
   enableACME = true;
   locations."/grafana/" = {

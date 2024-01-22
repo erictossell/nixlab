a3735b1578cd64bdfcc60a507a0ef884e11163af
Eric Tossell <erictossell@gmail.com>
2024-01-22 12:41:19 -0500

    Firewall

--- modules/grafana/default.nix
+++ modules/grafana/default.nix
@@ -6,9 +6,9 @@
       server = {
         http_addr = "0.0.0.0";
 	http_port = 3001;
-        domain = "graf.local";
-	root_url = "https://graf.local/grafana/"; 
-	serve_from_sub_path = true;
+        #domain = "graf.local";
+	#root_url = "https://graf.local/grafana/"; 
+	#serve_from_sub_path = true;
       };
     };
   };

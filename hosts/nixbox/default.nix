f6b5e4455f6e55ec0fd22acb69cced8d41ccfda5
Eric Tossell <erictossell@gmail.com>
2024-04-04 20:07:05 -0400

    22

--- hosts/nixbox/default.nix
+++ hosts/nixbox/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./hardware-configuration.nix
   ];
-  networking.firewall.allowedTCPPorts = [ 5006 8443 2222 ];
+  networking.firewall.allowedTCPPorts = [ 5006 8443 2222 22 ];
+  networking.firewall.allowedUDPPorts = [ 22 ];
 }

874dec28ef3e7f7286257d417b3e09505164a54a
Eric Tossell <erictossell@gmail.com>
2024-05-01 17:08:45 -0400

    feature:soft-serve

--- modules/soft-serve/default.nix
+++ modules/soft-serve/default.nix
@@ -5,13 +5,13 @@
       name = "E-Corp Git";
       log_format = "text";
       ssh = {
-        listen_addr = ":2222";
+        listen_addr = ":23231";
 	public_url = "eriim.dev";
 	max_timeout = 30;
 	idle_timeout = 120;
       };
-      stats.listen_addr = ":2222";
-      initial_admin_keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAETSTzRnvYIQsOwhdwcbVRyZVnP6/F3b+inurb9+RMu eriim" ];
+      stats.listen_addr = ":23233";
+      initial_admin_keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAETSTzRnvYIQsOwhdwcbVRyZVnP6/F3b+inurb9+RMu" ];
     };
   };
 }

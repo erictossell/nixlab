01afa547686870ce88bdf4add72f84d1b75bc18e
Eric Tossell <erictossell@gmail.com>
2024-05-01 17:20:28 -0400

    chore:ports

--- modules/soft-serve/default.nix
+++ modules/soft-serve/default.nix
@@ -14,4 +14,5 @@
       initial_admin_keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAETSTzRnvYIQsOwhdwcbVRyZVnP6/F3b+inurb9+RMu" ];
     };
   };
+  networking.firewall.allowedTCPPorts = [ 23231 23232 23233 ]; 
 }

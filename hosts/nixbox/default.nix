55cd024d02fa909b45449fc716b7e407330d07f8
Eric Tossell <erictossell@gmail.com>
2025-08-17 10:09:42 -0400

    chore: ports for DNS

--- hosts/nixbox/default.nix
+++ hosts/nixbox/default.nix
@@ -2,6 +2,6 @@
   imports = [
     ./hardware-configuration.nix
   ];
-  networking.firewall.allowedTCPPorts = [ 3104 4533 5006 8443 2222 22 ];
-  networking.firewall.allowedUDPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 53 5380 3104 4533 5006 8443 2222 22 ];
+  networking.firewall.allowedUDPPorts = [ 22 53 ];
 }

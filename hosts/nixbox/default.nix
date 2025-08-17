803b8e2a34f241448343397d37e1d0c21ba3caf5
Eric Tossell <erictossell@gmail.com>
2025-08-17 10:30:31 -0400

    chore: ports for DHCP

--- hosts/nixbox/default.nix
+++ hosts/nixbox/default.nix
@@ -3,5 +3,5 @@
     ./hardware-configuration.nix
   ];
   networking.firewall.allowedTCPPorts = [ 53 5380 3104 4533 5006 8443 2222 22 ];
-  networking.firewall.allowedUDPPorts = [ 22 53 ];
+  networking.firewall.allowedUDPPorts = [ 22 53 67 ];
 }

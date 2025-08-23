787e1d760477f7b4140548d1926e5f69616dbc35
Eric Tossell <erictossell@gmail.com>
2025-08-23 13:01:09 -0400

    specific IP firewall rule

--- modules/node_exporter/default.nix
+++ modules/node_exporter/default.nix
@@ -6,7 +6,7 @@
     enabledCollectors = [ "systemd" ];
     extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" ];
   };
-  networking.firewall.extraRules = ''
+  networking.firewall.extraCommands = ''
     iptables -A INPUT -p tcp --dport 9100 -s 192.168.2.15 -j ACCEPT
     iptables -A INPUT -p tcp --dport 9100 -j DROP
   '';

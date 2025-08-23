4ff3199af38c53d8976bfe4f0dfc2e3f5aeba28b
Eric Tossell <erictossell@gmail.com>
2025-08-22 21:50:19 -0400

    Ports on terra

--- hosts/terra/default.nix
+++ hosts/terra/default.nix
@@ -3,5 +3,5 @@
     ./hardware-configuration.nix
   ];
   networking.hostId = "4a98161d";
-  networking.firewall.allowedTCPPorts = [ 8096 ];
+  networking.firewall.allowedTCPPorts = [ 8096 9090 3000 ];
 }

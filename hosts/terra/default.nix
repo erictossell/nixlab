ada297a421d5bb63304b7083baf595d39e99da15
Eric Tossell <erictossell@gmail.com>
2025-08-23 13:08:16 -0400

    specific IP firewall rule

--- hosts/terra/default.nix
+++ hosts/terra/default.nix
@@ -4,4 +4,7 @@
   ];
   networking.hostId = "4a98161d";
   networking.firewall.allowedTCPPorts = [ 8096 9090 3000 ];
+  networking.firewall.extraCommands = ''
+          iptables -A INPUT -p tcp --dport 9100 -s 172.17.0.0/16 -j ACCEPT
+  '';
 }
